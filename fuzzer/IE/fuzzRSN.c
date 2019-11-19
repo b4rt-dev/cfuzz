49bf76d53362ab6c87cf0b149d217e20abe4bd82
Bart <bart@b4rt.nl>
2019-11-19 11:08:49 +0100

    Improved and finished RSN fuzzer

--- fuzzer/IE/fuzzRSN.c
+++ fuzzer/IE/fuzzRSN.c
@@ -11,9 +11,9 @@ Fuzzes rsn Information element
 int rsnRunningState = 0;
 
 //Number of fuzzing states
-const int rsnStates =  7;
+const int rsnStates =  10;
 //Steps of fuzzers for each fuzzing state
-const int rsnSteps[] =   {1, 4, 8, 8, 8, 8, 32};
+const int rsnSteps[] =   {1, 4, 8, 8, 8, 8, 32, 255, 16, 6};
 
 //Current state and step of the rsnFuzzer
 int fuzzState;
@@ -59,7 +59,22 @@ void rsnPrintCurrentState()
             printf("Fuzzing small lengths\n");
             break;
         }
-        case 7:
+        case 7: 
+        {
+            printf("Fuzzing Cipher suite type\n");
+            break;
+        }
+        case 8: 
+        {
+            printf("Fuzzing Cipher suite selector\n");
+            break;
+        }
+        case 9: 
+        {
+            printf("Fuzzing Cipher suite large lengths\n");
+            break;
+        }
+        case 10:
         {
             printf("\e[33mDone with fuzzing rsn\e[39m\n");
             break;
@@ -302,6 +317,266 @@ infoElem rsnFuzz()
                 rsn.data = data;
                 break;
             } 
+            case 7:
+            {
+                rsn.id = 48;
+                rsn.len = 42;
+                rsn.len_data = 42;
+                u_char *data = malloc(42);
+                u_char *valid = "\x01\x00\x00\x0f\xac\x02\x01\x00"
+                                "\x00\x0f\xac\xff\x01\x00"
+                                "\x00\x0f\xac\xff\x00\x00\x01\x00"
+                                "\xff\xff\xff\xff\xff\xff\xff\xff"
+                                "\xff\xff\xff\xff\xff\xff\xff\xff"
+                                "\x00\x0f\xac\xff";
+                memcpy(data, valid, 42);
+                data[11] = fuzzStep;
+                data[17] = fuzzStep;
+                int i;
+                for (i=0; i<16; i++)
+                {
+                    data[22+i] = fuzzStep;
+                }
+                data[41] = fuzzStep;
+                rsn.data = data;
+
+                break;
+            }
+            case 8:
+            {
+                if (fuzzStep < 8)
+                {
+                    rsn.id = 48;
+                    rsn.len = 42;
+                    rsn.len_data = 42;
+                    u_char *data = malloc(42);
+                    u_char *valid = "\x01\x00\x00\x0f\xac\x02\x01\x00"
+                                    "\xff\xff\xff\xff\x01\x00"
+                                    "\xff\xff\xff\xff\x00\x00\x01\x00"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff";
+                    memcpy(data, valid, 42);
+                    data[8] = fuzzStep;
+                    data[9] = fuzzStep;
+                    data[10] = fuzzStep;
+                    data[11] = fuzzStep;
+                    data[14] = fuzzStep;
+                    data[15] = fuzzStep;
+                    data[16] = fuzzStep;
+                    data[17] = fuzzStep;
+                    int i;
+                    for (i=0; i<20; i++)
+                    {
+                        data[22+i] = fuzzStep;
+                    }
+                    rsn.data = data;
+                }
+                else
+                {
+                    rsn.id = 48;
+                    rsn.len = 42;
+                    rsn.len_data = 42;
+                    u_char *data = malloc(42);
+                    u_char *valid = "\x01\x00\x00\x0f\xac\x02\x01\x00"
+                                    "\xff\xff\xff\xff\x01\x00"
+                                    "\xff\xff\xff\xff\x00\x00\x01\x00"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff";
+                    memcpy(data, valid, 42);
+                    data[8] = 255 - fuzzStep + 8;
+                    data[9] = 255 - fuzzStep + 8;
+                    data[10] = 255 - fuzzStep + 8;
+                    data[11] = 255 - fuzzStep + 8;
+                    data[14] = 255 - fuzzStep + 8;
+                    data[15] = 255 - fuzzStep + 8;
+                    data[16] = 255 - fuzzStep + 8;
+                    data[17] = 255 - fuzzStep + 8;
+                    int i;
+                    for (i=0; i<20; i++)
+                    {
+                        data[22+i] = 255 - fuzzStep + 8;
+                    }
+                    rsn.data = data;
+                }
+                break;
+            }
+            case 9:
+            {
+                if (fuzzStep == 0)
+                {
+                    rsn.id = 48;
+                    rsn.len = 252;
+                    rsn.len_data = 252;
+                    u_char *data = malloc(252);
+                    u_char *valid = "\x01\x00\x00\x0f\xac\x02"
+                                    "\x3D\x00"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04";
+                    memcpy(data, valid, 252);
+                    rsn.data = data;
+                }
+                if (fuzzStep == 1)
+                {
+                    rsn.id = 48;
+                    rsn.len = 252;
+                    rsn.len_data = 252;
+                    u_char *data = malloc(252);
+                    u_char *valid = "\x01\x00\x00\x0f\xac\x02"
+                                    "\x3D\x00"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff";
+                    memcpy(data, valid, 252);
+                    rsn.data = data;
+                }
+                if (fuzzStep == 2)
+                {
+                    rsn.id = 48;
+                    rsn.len = 254;
+                    rsn.len_data = 254;
+                    u_char *data = malloc(254);
+                    u_char *valid = "\x01\x00\x00\x0f\xac\x02"
+                                    "\x00\x00"
+                                    "\x3D\x00"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04";
+                    memcpy(data, valid, 252);
+                    rsn.data = data;
+                }    
+                if (fuzzStep == 3)
+                {
+                    rsn.id = 48;
+                    rsn.len = 254;
+                    rsn.len_data = 254;
+                    u_char *data = malloc(254);
+                    u_char *valid = "\x01\x00\x00\x0f\xac\x02"
+                                    "\x00\x00"
+                                    "\x3D\x00"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff";
+                    memcpy(data, valid, 252);
+                    rsn.data = data;
+                }    
+                if (fuzzStep == 4)
+                {
+                    rsn.id = 48;
+                    rsn.len = 254;
+                    rsn.len_data = 254;
+                    u_char *data = malloc(254);
+                    u_char *valid = "\x01\x00\x00\x0f\xac\x02"
+                                    "\x00\x00"
+                                    "\x00\x00"
+                                    "\x00\x00"
+                                    "\x0f\x00"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    "\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04\x00\x0f\xac\x04"
+                                    ;
+                    memcpy(data, valid, 252);
+                    rsn.data = data;
+                }    
+                if (fuzzStep == 5)
+                {
+                    rsn.id = 48;
+                    rsn.len = 254;
+                    rsn.len_data = 254;
+                    u_char *data = malloc(254);
+                    u_char *valid = "\x01\x00\x00\x0f\xac\x02"
+                                    "\x00\x00"
+                                    "\x00\x00"
+                                    "\x00\x00"
+                                    "\x0f\x00"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+                                    ;
+                    memcpy(data, valid, 252);
+                    rsn.data = data;
+                }    
+
+                break;
+            }
         }
     }
     
